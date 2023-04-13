fa9c1cdb5aaca369d57b5f254b637348a6958350
ALValdiviesoo <ledorvaldivieso@protonmail.com>
2023-04-13 14:45:56 +0800

    Reset code to (Make simple functions)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let currentNumber = "";
 let previousNumber = "";
 let operator = "";
-let again = false;
 
 let clear = document.querySelector('.clear');
 let equal = document.querySelector('.equals');
@@ -25,89 +24,40 @@ number.forEach((num) => {
 clear.onclick = () => {
   currentNumber = "";
   currentScreen.textContent = currentNumber;
-  previousNumber = "";
-  previousScreen.textContent = previousNumber;
 }
 
 operators.forEach((ope) => {
   ope.addEventListener("mousedown", () => {
-    if(currentScreen.textContent != "" && previousScreen.textContent != "") {
-      again = true;  
-      equalClick(ope.textContent);
-    } else {
-      operator = ope.textContent;
-      previousNumber = currentScreen.textContent;
-      previousScreen.textContent = `${previousNumber} ${operator}`;
-      currentNumber = "";
-      currentScreen.textContent = currentNumber;
-    }
+    operator = ope.textContent;
+    previousNumber = currentScreen.textContent;
+    previousScreen.textContent = `${previousNumber} ${operator}`;
+    currentNumber = "";
+    currentScreen.textContent = currentNumber;
   })
 })
 
-function equalClick(ope) {
+equal.onclick = () => {
   currentNumber = currentScreen.textContent;
 
   switch(operator) {
     case 'x':
-      if(again == true) {
-        previousScreen.textContent = `${(Number(currentNumber) * Number(previousNumber))} ${ope}`;
-        previousNumber = `${(Number(currentNumber) * Number(previousNumber))}`;
-        currentNumber = "";
-        currentScreen.textContent = currentNumber;
-      } else {
-        previousScreen.textContent = "";
-        currentScreen.textContent = (Number(currentNumber) * Number(previousNumber));
-      }
+      previousScreen.textContent = "";
+      currentScreen.textContent = (Number(currentNumber) * Number(previousNumber));
       break;
 
     case '-':
-      if(again == true) {
-        previousScreen.textContent = `${(Number(previousNumber) - Number(currentNumber))} ${ope}`;
-        previousNumber = `${(Number(previousNumber) - Number(currentNumber))}`;
-        currentNumber = "";
-        currentScreen.textContent = currentNumber;
-      } else {
-        previousScreen.textContent = "";
-        currentScreen.textContent = (Number(previousNumber) - Number(currentNumber));
-      }
+      previousScreen.textContent = "";
+      currentScreen.textContent = (Number(previousNumber) - Number(currentNumber));
       break;
 
     case '+':
-      if(again == true) {
-        previousScreen.textContent = `${(Number(currentNumber) + Number(previousNumber))} ${ope}`;
-        previousNumber = `${(Number(currentNumber) + Number(previousNumber))}`;
-        currentNumber = "";
-        currentScreen.textContent = currentNumber;
-      } else {
-        previousScreen.textContent = "";
-        currentScreen.textContent = (Number(currentNumber) + Number(previousNumber));
-      }
+      previousScreen.textContent = "";
+      currentScreen.textContent = (Number(currentNumber) + Number(previousNumber));
       break;
 
     case '/':
-      if(again == true) {
-        previousScreen.textContent = `${(Number(previousNumber) / Number(currentNumber))} ${ope}`;
-        previousNumber = `${(Number(previousNumber) / Number(currentNumber))}`;
-        currentNumber = "";
-        currentScreen.textContent = currentNumber;
-      } else {
-        previousScreen.textContent = "";
-        currentScreen.textContent = (Number(previousNumber) / Number(currentNumber));
-      }
+      previousScreen.textContent = "";
+      currentScreen.textContent = (Number(previousNumber) / Number(currentNumber));
       break;
-  }
-
-  currentNumber = "";  
-}
-
-equal.onclick = () => equalClick(operator);
-
-decimal.onclick = () => {
-  if (currentScreen.textContent == "") {
-    currentNumber += "0."
-    currentScreen.textContent = currentNumber;
-  } else {
-    currentNumber += decimal.textContent;
-    currentScreen.textContent = currentNumber;
   }
 }
